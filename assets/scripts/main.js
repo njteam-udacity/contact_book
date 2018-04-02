34a0e02e7322d320065f6aa1410cc09e4b743624
BreeGWG <queenkim3g@gmail.com>
2018-04-02 13:32:31 -0400

    Refactors main.js. Adds config file. Updates templates folder to store shared partials.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,131 +1,166 @@
-function initializeApplication() {
+// (function () {
+     /**
+     * @type {[key: string]: object}
+     */
+    var templates; //An object that will contain all of our template functions.
 
-    var templates = {}, //An object that will contain all of our html templates.
-        content = {} //An object that will contain our content.
+    /**
+     * @type {[key: string]: object}
+     */
+    var content; //An object that will contain our content.
 
-    getText("../templates/", ".html", ["welcome", "footer", "header"], function(error, tpls) {
+    function initializeApplication() {
 
-        if (error) return console.log(error);
-
-        templates = tpls;
-
-        getText("../data/", ".json", ["content", "contacts"], checkForError(function(data) {
-            content = parseJSON(data);
+        $.getJSON('/config.json', function (config) {
             
-            renderPage(templates.header, content);
-            renderPage(templates.welcome, content);
-            renderPage(templates.footer, content);
-        }));
+            getText("/templates/", ".html", config.templates, function(error, fileContents) {
+                 
+                if (error) return console.log(error);
 
-    })
-}
+                templates = {};
 
-/**
- * Utilize handlebars templating to bind an html template with 
- * content data, then used jQuery to render the page view.
- * @param {string} html fragment/template
- * @param {object} Object containing content data. 
- */
+                for (var filename in fileContents) {
+                    
+                    // textFileContent variable represent a layout 
+                    //or partial "source" in templates.
+                    var textFileContent = fileContents[filename];
+                    
+                    // templateFunction is the pre-compiled generated
+                    //template function returned from Handlebars.
+                    var templateFunction = Handlebars.compile(textFileContent); 
 
-function renderPage(source, data) {
+                    // templates[filename] stores template functions in the 
+                    // shared variable "templates" in order to be called through out the application.
+                    templates[filename] = templateFunction;
 
-    var template = Handlebars.compile(source);
-    var html = template(data.content);
+                    //while we are at it. If source is a partial we will register
+                    //that partial in handlebars as well. 
+                    if (filename.indexOf("partials/") === 0) {
+                        Handlebars.registerPartial(filename, textFileContent);
+                    }
 
-    $("body").append(html);
+                }
 
-}
+                getText("/content/en/", ".json", config.content, checkForError(function(data) {
+        
+                    content = parseJSON(data);
+                
+                    renderPage(templates, content);
+        
+                }));
+                
+            });
 
-/** 
- * Gathers data needed for the website
- * @param {string}  basePath path to templates folder. Must end with a 
- *                  forward slash character (/)
- * @param {string}  extension string file type.
- * @param {string[]}  filenames of file names.
- * @param {function} "callback" handler function   
- */
-function getText(basePath, extension, filenames, callback) {
-    
-    var contentMap = {}, //object containing all of the templates
-        i = 0; //iterator
-
-    function loop(name) {
-
-        i += 1;
-
-        var filepath = basePath + name + extension;
-
-        ajaxRequest("GET", filepath, function(e) {
-
-            if (e.target.status !== 200) {
-                return next(new Error('Could not load the file "' +
-                    filepath + '" from the server!'));
-            }
-
-            contentMap[name] = e.target.responseText;
-            next();
-            
         });
 
+
     }
 
-    function next(error) {
+    /**
+     * Utilize handlebars templating to bind an html template with 
+     * content data, then used jQuery to render the page view.
+     * @param {string} html fragment/template
+     * @param {object} Object containing content data. 
+     */
+
+    function renderPage(templates, data) {
+      debugger;
+        $("body").append(templates.welcome(data));
+      
+    }
+
+    /** 
+     * Gathers data needed for the website
+     * @param {string}  basePath path to templates folder. Must end with a 
+     *                  forward slash character (/)
+     * @param {string}  extension string file type.
+     * @param {string[]}  filenames of file names.
+     * @param {function} "callback" handler function   
+     */
+    function getText(basePath, extension, filenames, callback) {
         
-        if (error) {
-            return callback(error);
+        var contentMap = {}, //object containing all of the templates
+            i = 0; //iterator
+
+        function loop(name) {
+
+            i += 1;
+
+            var filepath = basePath + name + extension;
+
+            ajaxRequest("GET", filepath, function(e) {
+
+                if (e.target.status !== 200) {
+                    return next(new Error('Could not load the file "' +
+                        filepath + '" from the server!'));
+                }
+
+                contentMap[name] = e.target.responseText;
+                next();
+                
+            });
+
         }
 
-        if (i < filenames.length) {
-            loop(filenames[i]);
-        } else {
-            callback(null, contentMap);
+        function next(error) {
+            
+            if (error) {
+                return callback(error);
+            }
+
+            if (i < filenames.length) {
+                loop(filenames[i]);
+            } else {
+                callback(null, contentMap);
+            }
+            
         }
+
+        next();
+
+    }
+
+    function ajaxRequest(method, url, callback) {
+
+        var xhr = new XMLHttpRequest();
+
+        xhr.addEventListener('load', callback);
+        xhr.open(method, url);
+        xhr.send();
+
+    }
+
+    function parseJSON(dataObj) {
+        var parsedData = {};
+
+        try {
+            for (var property in dataObj) {
+                parsedData[property] = typeof (dataObj[property]) === "string" ?
+                    JSON.parse(dataObj[property]) : null;
+            }
+            return parsedData;
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    function checkForError(callback) {
+
+        return function(possibleError, expectedValue) {
+
+            if (possibleError) {
+                return console.error(possibleError);
+            }
+
+            callback(expectedValue);
+
+        }
+
+    }
+
+    $(document).ready(function () {
         
-    }
+        initializeApplication();
+    });
 
-    next();
-
-}
-
-function ajaxRequest(method, url, callback) {
-
-    var xhr = new XMLHttpRequest();
-
-    xhr.addEventListener('load', callback);
-    xhr.open(method, url);
-    xhr.send();
-
-}
-
-function parseJSON(dataObj) {
-    var parsedData = {};
-
-    try {
-        for (var property in dataObj) {
-            parsedData[property] = typeof (dataObj[property]) === "string" ?
-                JSON.parse(dataObj[property]) : null;
-        }
-        return parsedData;
-    } catch (error) {
-        console.error(error);
-    }
-}
-
-function checkForError(callback) {
-
-    return function(possibleError, expectedValue) {
-
-        if (possibleError) {
-            return console.error(possibleError);
-        }
-
-        callback(expectedValue);
-
-    }
-
-}
-
-$(document).ready(function () {
-
-    initializeApplication();
-});
+// });
